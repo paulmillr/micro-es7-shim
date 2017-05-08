b7f872aa7e7f124f11068ae40fc70519e1b7989e
Aleksey Shvayka <shvaikalesh@gmail.com>
2017-05-08 07:19:04 +0300

    remove `Array#includes` shim

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// https://bugs.chromium.org/p/v8/issues/detail?id=5059
+Array.prototype[Symbol.unscopables].includes = true;
+
 const install = (target, methods) => {
   Object.keys(methods).forEach(key => {
     if (target.hasOwnProperty(key)) return;
@@ -11,17 +14,7 @@ const install = (target, methods) => {
   });
 };
 
-install(Array.prototype, {
-  includes(search) {
-    if (this.length === 0) return false;
-    const array = arguments.length > 1 ? [].slice.call(this, arguments[1]) : this;
-    const is = Number.isNaN(search) ? Number.isNaN : item => item === search;
-    return [].findIndex.call(array, is) !== -1;
-  },
-});
 
-// https://bugs.chromium.org/p/v8/issues/detail?id=5059
-[][Symbol.unscopables].includes = true;
 
 install(Object, {
   values(object) {
